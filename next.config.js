9020918e152299f0a35b46bc7fbe41c6ae310c74
Ness Chu <35279510+justness@users.noreply.github.com>
2023-05-25 02:03:10 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+  // trailingSlash: true,
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
   images: {
-    remotePatterns: [
+    domains: [
       {
         hostname: "drive.google.com",
       },
     ],
   },
 };
+ 
+module.exports = nextConfig;
