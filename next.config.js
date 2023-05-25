56a61d6f016a68aa896fed34059fc548eb6a51c1
Ness Chu <35279510+justness@users.noreply.github.com>
2023-05-25 01:42:22 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    loader: 'akamai',
+    path: '',
     remotePatterns: [
       {
         hostname: "drive.google.com",
       },
     ],
   },
+  assetPrefix: './',
 }
 
 module.exports = nextConfig;
